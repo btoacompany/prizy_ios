d1997e406c55f4ee736c42e82284feb79f901cf6
Jr Fatalla <jrfatalla@gmail.com>
2016-10-29 23:58:55 +0900

    add playground for quick tests

---
+++ ios/prizy.me/prizy.me/Constants/Segue.swift
@@ -0,0 +1,4 @@
+enum Segue: String {
+    case SEGUE_LOGIN_TO_RECOVER="LoginRecover"
+    case SEGUE_LOGIN_TO_WEB="LoginWeb"
+}
---
+++ ios/prizy.me/prizy.me/ViewControllers/RecoverPasswordViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RecoverPasswordViewController.swift
+//  prizy.me
+//
+//  Created by Jay Rinaldi Fatalla on 2016/10/29.
+//  Copyright © 2016 Jay Rinaldi Fatalla. All rights reserved.
+//
+
+import Foundation
