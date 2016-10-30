cf46e31117bf98a0ebaff1607583c163b2fd33dd
Jr Fatalla <jrfatalla@gmail.com>
2016-10-30 12:24:19 +0900

    add logout mechanism add session for every request

--- ios/prizy.me/prizy.me/ViewControllers/WebViewController.swift
+++ ios/prizy.me/prizy.me/ViewControllers/WebViewController.swift
@@ -9,33 +9,82 @@
 import UIKit
 import WebKit
 
-class WebVC: UIViewController {
+class WebVC: UIViewController, WKNavigationDelegate {
     
     public var session:String = ""
     public weak var webview:WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loadWebView()
+    }
+
+    func loadWebView() {
         let webView = WKWebView()
         webView.frame = self.view.frame
+        
         self.view.addSubview(webView)
         self.webview = webView
-        self.webview.evaluateJavaScript("document.cookie='\(self.session)';domain='prizy.me';") { (data, error) -> Void in
+        self.webview.navigationDelegate = self
+        let js = "document.cookie='\(self.session)';domain='www.prizy.me';"
+        self.webview.evaluateJavaScript(js) { (data, error) -> Void in
             self.webview.load({
                 var urlRequest = URLRequest(url: URL(string: "https://www.prizy.me/user")!)
                 urlRequest.addValue(self.session, forHTTPHeaderField: "Cookie")
                 return urlRequest
                 }())
         }
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        
+    }
+    
+    func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url!
+        var request = navigationAction.request
+        if url.host == "www.prizy.me" || url.host == "prizy.me" {
+            if url.absoluteString.contains("prizy.me/login") {
+                decisionHandler(.cancel)
+                self.dismiss(animated: true, completion: nil)
+            }
+            else if request.value(forHTTPHeaderField: "Cookie") == nil {
+                request.addValue(self.session, forHTTPHeaderField: "Cookie")
+                decisionHandler(.cancel)
+                webView.load(request)
+            }
+        }
+        
+        decisionHandler(.allow)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        if let httpResponse = navigationResponse.response as? HTTPURLResponse {
+            let url = httpResponse.url!
+            if url.absoluteString.contains("www.prizy.me/login") {
+                decisionHandler(.cancel)
+                self.dismiss(animated: true, completion: nil)
+            }
+            decisionHandler(.allow)
+        }
+    }
     /*
     // MARK: - Navigation
 
